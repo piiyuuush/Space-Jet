d6976698369a5cdaf54a35d10d7f62a5306d045d
haziq <165279037+haziqlandge@users.noreply.github.com>
2025-01-11 13:21:52 +0530

    Update game.js
    Canvas updates to next frame and clears previous frames
    Also added jet movement

--- space/game.js
+++ space/game.js
@@ -3,6 +3,10 @@ let columns = 5;
 
 const tileSize = document.getElementById(`tile`).offsetHeight; // gets the size of 1 tile from an invisible div
 //ofsetHeight converts 4rem into pixels so for pc browser 4*16px
+
+let canvas= document.getElementById(`board`); 
+canvas.width = columns*tileSize; // setting up the canvas size
+canvas.height = rows*tileSize;
 let context; // this is our drawing variable
 
 let jet = {
@@ -11,17 +15,14 @@ let jet = {
     height: tileSize,
     width: tileSize
 } // jet object which stores its x and y coordinates and height and width
+jetImg = new Image(); // creating an img object for jet
+jetImg.src = "/jet.png"
 
+document.addEventListener(`keydown`, move);
 
-// when the game/window loads up we want to set up the board height n width and also start updating the frames
+// when the window loads we want the canvas to start drawing our game and update the frames
 window.onload = function(){
-    let canvas= document.getElementById(`board`); 
-    canvas.width = columns*tileSize; // setting up the canvas size
-    canvas.height = rows*tileSize;
-
     context = canvas.getContext(`2d`); //we want to make 2d canvas graphics
-    jetImg = new Image(); // creating an img object for our jet then drawing it
-    jetImg.src = "/jet.png"
     jetImg.onload = function() {
         context.drawImage(jetImg, jet.x,jet.y,jet.width,jet.height);
     }
@@ -29,6 +30,17 @@ window.onload = function(){
 }
 //constantly updates the postion(60fps usually)
 function update(){
+    context.clearRect(0, 0, canvas.width, canvas.height); // clear enitre board and update next frame
+    context.drawImage(jetImg, jet.x,jet.y,jet.width,jet.height); //draw jet then call next frame where canvas cleared and again drawn
     requestAnimationFrame(update);
-    context.drawImage(jetImg, jet.x,jet.y,jet.width,jet.height);
+}
+
+//defines the movement of jet, if key is pressed then it will move by one tile
+function move(e){
+    if(e.key === `ArrowLeft` && jet.x > 0){
+        jet.x -= tileSize;
+    }
+    else if(e.key === `ArrowRight` && jet.x <canvas.width - tileSize){
+        jet.x += tileSize;
+    }
 }
